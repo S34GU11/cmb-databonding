d922fe0d42ae59950b2f783fd42a3462dac33e39
Serhii Semeniuk <sergsemeniuk@gmail.com>
2023-02-14 16:25:35 +0200

    output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'testServer', content: 'test Content'}];
+  serverElements = [];
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 
+  onBlueprintAdded(bluePrintDate: { blueprintName: string; blueprintContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintDate.blueprintName,
+      content: bluePrintDate.blueprintContent
+    });
+  }
 }
